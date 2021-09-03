8ad84af863ac355a8d6008549eb6671ccba1d111
tomvapon <tomasvarona91@gmail.com>
2021-09-03 18:00:28 +0200

    Add test.py, and logic for validators and snake_utils. Explain the resolution strategy in main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+This is the main file of the snake algorithm
+The problem is to be solve recursively based on
+a backtracking algorithm. In each configuration (board, snake)
+there are four possible movements (L, R, U, D). All options are
+explore and invalid solutions are discard.
+"""
+
